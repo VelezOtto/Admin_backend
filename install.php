4ff82d8d70b2de3653b28b5c930e2bb36309a39b
VelezOtto <velez.otto1993@gmail.com>
2016-08-16 16:04:20 +0200

    Update install.php

--- install.php
+++ install.php
@@ -1,35 +1,52 @@
-<?
-CREATE TABLE IF NOT EXISTS `webshop` (
-  `product_id` int(11) NOT NULL,
-  `product_name` varchar(45) NOT NULL,
-  `product_category` text(255) NOT NULL,
-  `product_description` text(255) NOT NULL,
-  `product_price` int(11) NOT NULL,
-  `product_condition` set('Used','Renewed','New') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+<?php 
 
-ALTER TABLE `products`
- ADD PRIMARY KEY (`product_id`);
- 
- ALTER TABLE `products`
-MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+$link = mysqli_connect("localhost", "root", "", "");
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql = "CREATE DATABASE IF NOT EXISTS webshop";
+if(mysqli_query($link, $sql)){
+    echo "Database webshop  created successfully.\n";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
 
 
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Camera','Fashion','Takes nice pictures','150','New');
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Laptop','Laptop','Standart multimedia laptop','300','Renewed');
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Shirt','Fashion','Made out of quality silk','50','Used');
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Photoshop Lessons','Software, Book','The way to learn Photoshop','100','New');
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Edible underwear','Fashion, Food','Not sure about this one','20','New');
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Dummy Name','Fashion, Laptop, Food','Dummy Description','150','Used');
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Dummy Name2','Fashion, Laptop, Food','Dummy Description2','150','Used');
-INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
-VALUES ('Dummy Name3','Fashion, Laptop, Food','Dummy Description3','150','Used');
+$link = mysqli_connect("localhost", "root", "", "webshop");
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+
+$sql= "CREATE TABLE IF NOT EXISTS products (
+  product_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  product_name varchar(45) NOT NULL,
+  product_category text(255) NOT NULL,
+  product_description text(255) NOT NULL,
+  product_price int(11) NOT NULL,
+  product_condition set('Used','Renewed','New') NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1";
+
+
+$sql = "INSERT INTO products (product_name, product_category, product_description, product_price, product_condition)
+VALUES ('Camera','Fashion','Takes nice pictures','150','New'),
+		('Laptop','Laptop','Standart multimedia laptop','300','Renewed'),
+		('Shirt','Fashion','Made out of quality silk','50','Used'),
+		('Photoshop Lessons','Software, Book','The way to learn Photoshop','100','New'),
+		('Edible underwear','Fashion, Food','Not sure about this one','20','New'),
+		('Dummy Name','Fashion, Laptop, Food','Dummy Description','150','Used'),
+		('Dummy Name2','Fashion, Laptop, Food','Dummy Description2','150','Used'),
+		('Dummy Name3','Fashion, Laptop, Food','Dummy Description3','150','Used');";
+
+if (mysqli_query($link, $sql)){
+    echo "Table products created successfully.";
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+mysqli_close($link);
+
 
 ?>
